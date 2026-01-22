b493b8dc42186d02bf7998de1dd51d872bbc02ad
SeongHo Kim <kkb053404@gmail.com>
2026-01-22 14:36:43 +0900

    fix: PEF8 규칙 적용: __all__ 뒤에 공백 추가

--- src/app/chatbot/services/chatbot_service.py
+++ src/app/chatbot/services/chatbot_service.py
@@ -1,7 +1,6 @@
 from app.chatbot.repository.chatbot_repository import ChatbotRepository
 from app.chatbot.services.base_service import BaseService
 
-
 class ChatbotService(BaseService):
     def __init__(self, chatbot_repository: ChatbotRepository):
         self.chatbot_repository= chatbot_repository
