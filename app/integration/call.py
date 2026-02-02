02e54f6a93ab59f3e8474b0c31ea73d88de0c56f
SeongHo Kim <kkb053404@gmail.com>
2026-02-02 13:19:14 +0900

    feat: s3 audio save

--- app/integration/call.py
+++ app/integration/call.py
@@ -8,27 +8,19 @@ class CALL:
         self.number = number
         self.silverlink_number = silverlink_number
         
-    def calling(self, elderly_id: int, phone_number: str, elderly_name: str) -> None:
-        import urllib.parse
+    def calling(self) -> None:
         # 계정 정보
         account_sid = self.account_sid
         auth_token = self.auth_token
         client = Client(account_sid, auth_token)
 
-        # 데이터를 URL 쿼리 파라미터로 추가
-        params = {
-            "elderly_id": elderly_id,
-            "elderly_name": elderly_name
-        }
-        query_string = urllib.parse.urlencode(params)
-        my_server_url = f"{self.url}/api/callbot/voice?{query_string}"
+        # ngrok 주소 뒤에 /voice 라우트를 붙여줍니다.
+        # 예: https://abcd-1234.ngrok-free.app/voice
+        my_server_url = f"{self.url}/api/callbot/voice"
 
         client.calls.create(
-            to=phone_number,      # 받는 사람 번호
+            to='+821053915653',      # 받는 사람 번호
             from_=self.silverlink_number,    # Twilio 발신 번호
-            url=my_server_url,        # 우리가 만든 AI 서버 주소
-            status_callback=f"{self.url}/api/callbot/status",
-            status_callback_event=["completed"],
-            record=True
+            url=my_server_url        # 우리가 만든 AI 서버 주소
         )
         
